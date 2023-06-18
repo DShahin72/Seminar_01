f648741dbb7633b3146c52ee4703679587b50432
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-18 13:52:13 +0300

    Возведение числа в квадрат

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Возвести введенное число в квадрат
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Квадрат заданного числа равен "+sqr);
+System.Console.WriteLine($"Квадрат равен {sqr}");
