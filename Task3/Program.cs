1aaaf11017b1fbd5a5a4e802b80a00a52ed78bcf
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-18 17:14:02 +0300

    Вывод дня недели

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,43 @@
-﻿// 
+﻿// Вывести день недели по заданному числу
+System.Console.Write("Введите номер дня недели от 1 до 7: ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+
+if (daynumber == 1) 
+{
+    System.Console.WriteLine("Это понедельник!");
+}
+
+else if (daynumber == 2) 
+{
+    System.Console.WriteLine("Это вторник!");
+}
+    
+else if (daynumber == 3) 
+{
+    System.Console.WriteLine("Это среда!");
+}
+
+else if (daynumber == 4)
+{
+    System.Console.WriteLine("Это четверг!");
+}
+
+else if (daynumber == 5)
+{
+    System.Console.WriteLine("Это пятница!");
+}
+
+else if (daynumber == 6)
+{
+    System.Console.WriteLine("Это суббота!");
+}
+
+else if (daynumber == 7)
+{
+    System.Console.WriteLine("Это воскресенье!");
+}
+
+else
+{
+    System.Console.WriteLine("Вы ввели неверное число");
+}
