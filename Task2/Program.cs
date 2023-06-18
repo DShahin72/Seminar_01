eb28a5a2d7f337ea241753ea15024020f1a31c4e
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-18 16:19:33 +0300

    Первое число - квадрат второго

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Является ли первое введенное число квадратом второго
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b*b)
+{
+    System.Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} не является квадратом числа {b}");
+}
