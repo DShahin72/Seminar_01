0a28def806ab650d9ff1e33b316affa60217ddc0
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 16:53:03 +0300

    Решение задачи через цикл for

---
+++ Task4 (цикл for)/Program.cs
@@ -0,0 +1,9 @@
+﻿// Составить программу, которая на вход принимает число N и выдает все целые числа от -N до N
+
+System.Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -N; i <= N; i++)
+{
+    System.Console.Write(i + " ");
+}
