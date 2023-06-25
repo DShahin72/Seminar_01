83573333ca680e6dbcb135e5c80a72c1d809f53d
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 16:43:29 +0300

    Вывод чисел от -N до N

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Составить программу, которая на вход принимает число N и выдает все целые числа от -N до N
+
+System.Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+while (i <= N)
+{
+    System.Console.Write(i + " ");
+    i++;
+}
