27cd52bd5765c024c12473e836f7a79d826b41f5
Dmitry Shakhin <dshahin@yandex.ru>
2023-06-25 17:52:59 +0300

    Добавлена проверка на трехзначное число

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -2,4 +2,12 @@
 
 System.Console.Write("Введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(N%10);
+
+if (N>99 && N<1000)
+{
+    System.Console.WriteLine(N%10);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели нетрехзначное число!");
+}
